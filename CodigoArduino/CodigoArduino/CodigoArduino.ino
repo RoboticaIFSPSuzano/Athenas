a4ad2c20f0528fc1723f8507a7aa164937dfef3c
Robótica <Robtica@PESQUISA-PC>
2018-09-17 19:32:37 -0300

    Teste

---
+++ CodigoArduino/CodigoArduino/CodigoArduino.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		CodigoArduino.ino
+ Created:	17/09/2018 19:28:58
+ Author:	Robótica
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
